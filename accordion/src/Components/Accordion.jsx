712a0416a17ace25ca61cd7359f0c9119ac3a1ea
Aman Kumar <amankarguwal0@gmail.com>
2024-07-06 18:08:34 +0530

    feat: Refactor AccordionContainer to use useEffect for lazy loading images

--- accordion/src/Components/Accordion.jsx
+++ accordion/src/Components/Accordion.jsx
@@ -1,20 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Accordion from "./Accordion";
 
-function Accordion({ index, item }) {
+function AccordionContainer({ data, setLimit }) {
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // console.log(entries);
+        if (entries[0].isIntersecting) {
+          observer.unobserve(entries[0].target);
+          setLimit((prevLimit) => prevLimit + 5);
+        }
+      },
+      { threshold: 0.6 }
+    );
+
+    const lastAccordion = document.querySelector(".accordion:last-child");
+    if (!lastAccordion) {
+      return;
+    }
+    observer.observe(lastAccordion);
+
+    return () => {
+      if (lastAccordion) observer.unobserve(lastAccordion);
+      observer.disconnect();
+    };
+  }, [data, setLimit]);
+
   const [show, setShow] = useState(false);
+
   return (
-    <div className="accordion">
-      <h1>
-        {item.question}
-        <span
-        onClick={() => setShow(!show)}
-        >
-            {show ? "➖" : "➕"}
-        </span>
-      </h1>
-      {show ? <h4>{item.answer}</h4> : null}
+    <div>
+      {data.map((item, index) => (
+        <div className="accordion" key={index}>
+          <h1>
+            {item.question}
+            <span onClick={() => setShow(!show)}>{show ? "➖" : "➕"}</span>
+          </h1>
+          {show ? <h4>{item.answer}</h4> : null}
+        </div>
+      ))}
     </div>
   );
 }
 
-export default Accordion;
+export default AccordionContainer;
