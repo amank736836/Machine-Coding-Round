70bbadd647c62154cecc9ac196f214350ffacd4e
Aman Kumar <amankarguwal0@gmail.com>
2024-07-06 18:02:59 +0530

    feat: Add Faq component for displaying frequently asked questions

---
+++ accordion/src/Components/Faq.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Accordion from "./Accordion";
+import data from "../data.json";
+import AccordionContainer from "./AccordionContainer";
+
+function Faq() {
+  console.log(data);
+  const [mainData, setMainData] = useState([]);
+  const [limit, setLimit] = useState(5);
+  
+  useEffect(() => {
+    const fetchData = data.faqs.filter((item, index) => index < limit);
+    setMainData(fetchData);
+    console.log(fetchData);
+  }, [limit]);
+
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <AccordionContainer data={mainData} setLimit={setLimit} />
+    </div>
+  );
+}
+
+export default Faq;
