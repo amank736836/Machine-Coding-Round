20df90c89eeb672ea923379b83b363482fdae437
Aman Kumar <amankarguwal0@gmail.com>
2024-08-04 22:50:47 +0530

    refactor: Remove logo import and unused code in App.js

--- search-bar/src/App.js
+++ search-bar/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import TypeAhead from './Components/TypeAhead';
 
--- search-bar/src/Components/TypeAhead.jsx
+++ search-bar/src/Components/TypeAhead.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const STATE = {
   LOADING: "LOADING",
@@ -10,19 +10,46 @@ export default function TypeAhead() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [status, setStatus] = useState(STATE.LOADING);
-  console.log(results);
+  const cache = useRef({});
+  //   console.log(cache);
+  //   console.log(results);
 
   useEffect(() => {
+    const abortController = new AbortController();
+    const { signal } = abortController;
     const fetchData = async () => {
-      const response = await fetch(
-        `https://dummyjson.com/products/search?q=${query}&limit=5`
-      );
-      //   console.log(await response.json());
-      const data = await response.json();
-      //   console.log(data);
-      setResults(data.products);
+      console.log("fetchData");
+      try {
+        if (cache.current[query]) {
+          console.log("cache");
+          setResults(cache.current[query]);
+          setStatus(STATE.SUCCESS);
+          return;
+        }
+        setStatus(STATE.LOADING);
+        const response = await fetch(
+          `https://dummyjson.com/products/search?q=${query}&limit=5`,
+          {
+            signal,
+          }
+        );
+        //   console.log(await response.json());
+        const data = await response.json();
+        //   console.log(data);
+        cache.current[query] = data.products;
+        setResults(data.products);
+        setStatus(STATE.SUCCESS);
+      } catch (error) {
+        // console.log(error);
+        if (error.name !== "AbortError") setStatus(STATE.ERROR);
+      }
     };
-    fetchData();
+    // fetchData();
+    const timerId = setTimeout(fetchData, 1000);
+    return () => {
+      clearTimeout(timerId);
+      abortController.abort();
+    };
   }, [query]);
 
   return (
@@ -33,11 +60,15 @@ export default function TypeAhead() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul>
-        {results.map((product) => {
-          return <li key={product.id}>{product.title}</li>;
-        })}
-      </ul>
+      {status === STATE.LOADING && <div>Loading...</div>}
+      {status === STATE.ERROR && <div>Something went wrong...</div>}
+      {status === STATE.SUCCESS && (
+        <ul>
+          {results.map((product) => {
+            return <li key={product.id}>{product.title}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
