c9c1061b069c33a43cbe9642b4414e52a82dda68
Aman Kumar <amankarguwal0@gmail.com>
2024-08-05 22:46:57 +0530

    style: Update container layout and cell styling in App.css

---
+++ toast/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToastContainer from './Components/ToastContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer/>
+    </div>
+  );
+}
+
+export default App;
