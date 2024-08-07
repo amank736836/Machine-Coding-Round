a04ea5971d4d94b4427e55270b6d7792811ddf62
Aman Kumar <amankarguwal0@gmail.com>
2024-08-07 22:18:41 +0530

    refactor: Add highestScoreSend function to send user's highest score to the server

---
+++ snake-game/src/Functions/highestScoreSend.js
@@ -0,0 +1,20 @@
+const highestScoreSend = async ({ user, score }) => {
+  const data = fetch(
+    "https://machine-coding-round-bsrq.onrender.com/snakeGame",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: user,
+        score: score,
+      }),
+    }
+  ).then((response) => {
+    console.log(response);
+    console.log("Data sent");
+  });
+};
+
+export default highestScoreSend;
