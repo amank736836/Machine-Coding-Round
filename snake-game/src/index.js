e15abdcbf24e565084304f408ae969e15e0107a6
Aman Kumar <amankarguwal0@gmail.com>
2024-08-08 15:27:02 +0530

    refactor: Update touch and gesture event listeners to prevent scrolling and gestures on the game screen

--- snake-game/src/index.js
+++ snake-game/src/index.js
@@ -3,29 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-document.addEventListener("gesturestart", function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener("gesturechange", function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener("gestureend", function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener("touchmove", function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener("touchstart", function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener("touchend", function (e) {
-  e.preventDefault();
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
