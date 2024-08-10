83c32ed658f8d9eedee48f67a88a458a6f2f7d88
Aman Kumar <amankarguwal0@gmail.com>
2024-08-10 23:25:04 +0530

    refactor: Optimize App.js by implementing virtualized list component

--- virtualized-list/src/App.js
+++ virtualized-list/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import VirtualizedList from "./Components/VirtualizedList";
 
-function App() {
+const LIST = Array.from({ length: 100000 }).map((_, index) => {
+  return index + 1;
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VirtualizedList list={LIST} width={300} height={400} itemHeight={40} />
   );
-}
+};
 
 export default App;
