3fd9241d984a1f18b438496559b1ede4162a7156
Aman Kumar <amankarguwal0@gmail.com>
2024-09-26 16:22:51 +0530

    Refactor: Update App.js to use CommentsContext for managing comments

--- nested-comments/src/App.js
+++ nested-comments/src/App.js
@@ -1,51 +1,10 @@
 import "./App.css";
 import CommentBox from "./Components/CommentBox";
-import { useState } from "react";
-import commentsData from "./commentsData.json";
+import { useCommentsContext } from "./Components/commentsContext";
 
 function App() {
-  const [comments, setComments] = useState(commentsData.comments);
-
-  const addComment = (value, parentId) => {
-    const newId = Date.now();
-    const newComment = {
-      id: newId,
-      parentId,
-      value,
-      children: [],
-    };
-    setComments((prevComments) => {
-      prevComments[parentId].children.push(newId);
-      return { ...prevComments, [newId]: newComment };
-    });
-  };
-
-  const deleteComment = (id) => {
-    const parentId = comments[id].parentId;
-    setComments((prevComments) => {
-      const newComments = { ...prevComments };
-      if (parentId) {
-        const parent = newComments[parentId];
-        parent.children = parent.children.filter((childId) => childId !== id);
-      }
-      const queue = [id];
-      while (queue.length) {
-        const currentId = queue.pop();
-        queue.push(...newComments[currentId].children);
-        delete newComments[currentId];
-      }
-      return newComments;
-    });
-  };
-
-  return (
-    <CommentBox
-      comment={comments[1]}
-      allComments={comments}
-      addComment={addComment}
-      deleteComment={deleteComment}
-    />
-  );
+  const { comments } = useCommentsContext();
+  return <CommentBox comment={comments[1]} />;
 }
 
 export default App;
