fb64590d2e6eb84861d2f263745730ba63222b8f
Aman Kumar <amankarguwal0@gmail.com>
2024-09-26 21:35:19 +0530

    Refactor: Update App.js to fix JSX syntax error

--- file-explorer/src/App.js
+++ file-explorer/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import FileExplorer from "./Components/FileExplorer";
 import FileExplorerContextWrapper from "./context/FileExplorerContext";
 function App() {
-  return;
-  <FileExplorerContextWrapper>
-    <FileExplorer id={1} />;
-  </FileExplorerContextWrapper>;
+  
+  return (
+    <FileExplorerContextWrapper>
+      <FileExplorer id={1} />
+    </FileExplorerContextWrapper>
+  );
 }
 
 export default App;
