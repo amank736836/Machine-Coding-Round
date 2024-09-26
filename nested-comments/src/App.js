4e7422646a96b1a0af90458945d6020a83444ea1
Aman Kumar <amankarguwal0@gmail.com>
2024-09-26 18:34:10 +0530

    Refactor: Update App.js to pass comment ID as prop to CommentBox

--- nested-comments/src/App.js
+++ nested-comments/src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 import CommentBox from "./Components/CommentBox";
-import { useCommentsContext } from "./Components/commentsContext";
 
 function App() {
-  const { comments } = useCommentsContext();
-  return <CommentBox comment={comments[1]} />;
+  return <CommentBox id={1} />;
 }
 
 export default App;
