a1ef2c879c5da9e66dd3093e014cb832a00644fc
Aman Kumar <amankarguwal0@gmail.com>
2024-09-26 18:34:17 +0530

    Refactor: Update import path for CommentsProvider in index.js

--- nested-comments/src/index.js
+++ nested-comments/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { CommentsProvider } from "./Components/commentsContext";
+import { CommentsProvider } from "./Context/commentsContext";
 import commentsData from "./commentsData.json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
