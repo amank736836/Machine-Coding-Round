e3b2b8a5803afabfda7f27ebf137214cb48e18a9
Aman Kumar <amankarguwal0@gmail.com>
2024-09-26 18:36:15 +0530

    Refactor: Update import path for FileExplorerContext in FileExplorer.jsx

--- file-explorer/src/Components/FileExplorer.jsx
+++ file-explorer/src/Components/FileExplorer.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { FileExplorerContext } from "../context/FileExplorerContext";
 
-function FileExplorer({ folderData }) {
-//   console.log(folderData);
-const [showChildren , setShowChildren] = useState(false);
+function FileExplorer({ id }) {
+  const { nodes: data } = useContext(FileExplorerContext);
+  const [showChildren, setShowChildren] = useState(false);
   return (
     <div className="container">
-      <h3
-      onClick={() => setShowChildren(!showChildren)}
-      >
-        {folderData.type === "folder" ? (showChildren ? "🔽 📂" : "▶️ 📁") : "📄"}
-        <span>{folderData.name}</span>
+      <h3 onClick={() => setShowChildren(!showChildren)}>
+        {data[id].type === "folder" ? (showChildren ? "🔽 📂" : "▶️ 📁") : "📄"}
+        <span>{data[id].name}</span>
       </h3>
-      { showChildren &&
-        folderData?.children?.map((childData , index) => {
-            return <FileExplorer key = {index} folderData = {childData} />
-        })
-      }
+      {showChildren &&
+        data[id]?.children?.map((childId, index) => {
+          return <FileExplorer key={index} id={childId} />;
+        })}
     </div>
   );
 }
