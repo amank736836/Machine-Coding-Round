9d0678e549e3f33ced9c65b58566cb9668b6c6ad
Aman Kumar <amankarguwal0@gmail.com>
2024-09-30 16:37:19 +0530

    Refactor: Remove console.log statement in Faq component

--- accordion/src/Components/Faq.jsx
+++ accordion/src/Components/Faq.jsx
@@ -4,14 +4,12 @@ import data from "../data.json";
 import AccordionContainer from "./Accordion";
 
 function Faq() {
-  console.log(data);
   const [mainData, setMainData] = useState([]);
   const [limit, setLimit] = useState(5);
 
   useEffect(() => {
     const fetchData = data.faqs.filter((item, index) => index < limit);
     setMainData(fetchData);
-    console.log(fetchData);
   }, [limit]);
 
   return (
